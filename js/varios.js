0a8726138b9b21caa8463ed8b3ed880b58db34a1
santiagojonnathan <99350597+santiagojonnathan@users.noreply.github.com>
2022-04-29 16:22:58 -0500

    Add files via upload

---
+++ js/varios.js
@@ -0,0 +1,23 @@
+﻿function evaluaD(oNombre, oApePat, oEail, oTel){
+	var sErr="";
+	var bRet = false;
+	
+		if (oNombre.value=="")
+			sErr= sErr + "\n Falta nombre";
+	
+		if (oApePat.value=="")
+			sErr= sErr + "\n Falta apellido paterno";
+	
+		if (oEail,value=="")
+			sErr= sErr + "\n Falta email";
+	
+		if (oTel.value=="")
+			sErr= sErr + "\n Falta telefono de contacto";
+		
+		if (sErr == "")
+			bRet = true;
+		else
+			alert(sErr);
+		
+		return bRet;
+	}
